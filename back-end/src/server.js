aa064305634cbba99d707249c56742e4d0e05467
Muhammad Taha <Muhammad.Taha@soc-solution.com>
2024-11-29 19:46:59 +0500

    feat: add email in back-end

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/api/send-email", async (req, res) => {
+
   const { fullName, companyName, title, email, contactNumber } = req.body;
 
   // Create a transporter for Nodemailer (make sure to configure with your SMTP provider)
@@ -25,8 +26,8 @@ app.post("/api/send-email", async (req, res) => {
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
-    to: email, // Send to the user's email
-    subject: "Welcome to the Pre-Launch Beta!",
+    to: `ben@wearevenu.com , oscar@wearevenu.com`, // Send to the user's email
+    subject: "New signup!",
     text: `
             Thank you for signing up for our Pre-Launch Beta!
 
@@ -37,7 +38,7 @@ app.post("/api/send-email", async (req, res) => {
             - Email: ${email}
             - Contact Number: ${contactNumber || "Not provided"}
 
-            We are excited to have you on board!
+            One more user signup for the venu beta!
         `,
   };
 
